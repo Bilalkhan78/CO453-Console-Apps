0a81bf8ac00c4d96970fb7febc73eec532c7f623
Bilal Khan <100128750@student.burnley.ac.uk>
2022-02-25 12:00:13 +0000

    Distamce converter

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,13 +1,45 @@
-﻿namespace ConsoleAppProject.App01
+﻿using System;
+
+namespace ConsoleAppProject.App01
 {
     /// <summary>
     /// Please describe the main features of this App
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Bilal Khan version 0.1
     /// </author>
     public class DistanceConverter
     {
+        private double Miles;
+        private double feet;
+        /// <summary>
+        /// 
+        /// </summary>
+        private void  Run()
+        {
+            InputMiles();
+            CalculateFeet();
+            Outputfeet();
+        }
+        /// <summary>
+        /// promt the user to enter the distance in miles 
+        /// input the miles as a double number
+        /// </summary>
+        private void InputMiles()
+        {
+            Console.Write("please enter the number of miles");
+            string value = Console.ReadLine();
+            Miles = Convert.ToDouble(value);
+        }
+        private void CalculateFeet()
+        {
+
+        }
+        private void  Outputfeet()
+        { 
+        
+        }
 
     }
+
 }
